7fbbc0152d1124c90ea09beb28167eb69a88c2be
suphot <suphotsudsee@gmail.com>
2025-12-15 08:54:45 +0700

    cors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Allow dev cross-origin requests from the mobile host used in this project.
-  allowedDevOrigins: ["https://mhs-mobile.phoubon.in.th"],
+  // Allow dev cross-origin requests from the mobile host used in this project (http + https).
+  allowedDevOrigins: [
+    "https://mhs-mobile.phoubon.in.th",
+    "http://mhs-mobile.phoubon.in.th",
+  ],
 };
 
 export default nextConfig;
